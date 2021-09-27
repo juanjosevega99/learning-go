3f574a51431b10c38a4a761ad4deb300126025b7
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2021-09-27 18:04:41 -0500

    first clase

--- src/main.go
+++ src/main.go
@@ -3,5 +3,31 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World, testing github 2")
+	// Statement of constants
+	const pi float64 = 3.14
+	const pi2 = 3.14
+
+	fmt.Println("Pi", pi)
+	fmt.Println("Pi2", pi2)
+
+	// Declaration of integer constants
+	base := 12
+	var height int = 14
+	var area int
+
+	fmt.Println(base, height, area)
+
+	// Zero values
+	var a int
+	var b float64
+	var c string
+	var d bool
+
+	fmt.Println(a, b, c, d)
+
+	// Square Area
+	const baseSquare = 10
+	squareArea := baseSquare * baseSquare
+	fmt.Println("Square area:", squareArea)
+
 }
